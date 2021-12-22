0301bc9ab893fa66a1b5730a37352015db0e8244
erdemgocen44 <78229631+erdemgocen44@users.noreply.github.com>
2021-12-22 22:24:29 +0100

    tamam

---
+++ src/main/java/com/lecture/car_rental/controller/FileDBController.java
@@ -0,0 +1,42 @@
+package com.lecture.car_rental.controller;
+
+
+import com.lecture.car_rental.domain.FileDB;
+import com.lecture.car_rental.service.FileDBService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+@AllArgsConstructor
+@RestController
+@CrossOrigin("http://localhost:8081")
+@RequestMapping(path = "/files")
+public class FileDBController {
+
+    private final FileDBService fileDBService;
+
+    @PostMapping("/upload")
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<Map<String, String>> uploadFile(@RequestParam("file") MultipartFile file) {
+
+        try {
+
+            FileDB fileDB = fileDBService.store(file);
+            Map<String, String> map = new HashMap<>();
+            map.put("imageId", fileDB.getId());
+
+            return ResponseEntity.status(HttpStatus.OK).body(map);
+
+        } catch (IOException e) {
+
+            Map<String, String> map = new HashMap<>();
+            map.put("message", "Could not upload the file: " + file.getOriginalFilename() + "!");
+            return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).body(map);
+        }
+    }
+}
