0f130ab2879e584d1299878cdb1b37136fbffacd
erdemgocen44 <78229631+erdemgocen44@users.noreply.github.com>
2021-12-24 19:47:07 +0100

    CarRepository tamam

--- src/main/java/com/lecture/car_rental/repository/CarRepository.java
+++ src/main/java/com/lecture/car_rental/repository/CarRepository.java
@@ -1,13 +1,16 @@
 package com.lecture.car_rental.repository;
 
 import com.lecture.car_rental.domain.Car;
+import com.lecture.car_rental.domain.dto.CarDTO;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
-
+import java.util.List;
 @Repository
 @Transactional
-public interface CarRepository extends JpaRepository<Car,Long> {
+public interface CarRepository extends JpaRepository<Car, Long> {
 
-
+    @Query("SELECT new com.lecture.car_rental.dto.CarDTO(c) FROM Car c")
+    List<CarDTO> findAllCar();
 }
