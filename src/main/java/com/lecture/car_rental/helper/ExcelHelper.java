3468b8b91be00405ab32ffc654c85b85569ac34e
erdemgocen44 <78229631+erdemgocen44@users.noreply.github.com>
2021-12-28 21:26:34 +0100

    Excel helper tamam

--- src/main/java/com/lecture/car_rental/helper/ExcelHelper.java
+++ src/main/java/com/lecture/car_rental/helper/ExcelHelper.java
@@ -1,5 +1,6 @@
 package com.lecture.car_rental.helper;
 
+import com.lecture.car_rental.domain.Car;
 import com.lecture.car_rental.domain.User;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
@@ -16,6 +17,12 @@ public class ExcelHelper {
     public static String TYPE = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
     static String[] HEADERS_USER = {"Id", "FirstName", "LastName", "PhoneNumber", "Email", "Address", "ZipCode", "Roles"};
     static String SHEET_USER = "Customers";
+
+    static String[] HEADERS_CAR = {"Id", "Model", "Doors", "Seats", "Luggage", "Transmission", "AirConditioning",
+            "Age", "pricePerHour", "FuelType"};
+    static String SHEET_CAR = "Cars";
+
+
 
     public static ByteArrayInputStream usersExcel(List<User> users) {
         try (Workbook workbook = new XSSFWorkbook();
@@ -47,6 +54,40 @@ public class ExcelHelper {
             return new ByteArrayInputStream(out.toByteArray());
 
         } catch (IOException e) {
+            throw new RuntimeException("fail to import data to Excel file: " + e.getMessage());
+        }
+    }
+
+    public static ByteArrayInputStream carsExcel(List<Car> cars) {
+        try (Workbook workbook = new XSSFWorkbook();
+             ByteArrayOutputStream out = new ByteArrayOutputStream()){
+
+            Sheet sheet = workbook.createSheet(SHEET_CAR);
+            Row headerRow = sheet.createRow(0);
+
+            for (int column = 0; column < HEADERS_CAR.length; column++) {
+                Cell cell = headerRow.createCell(column);
+                cell.setCellValue(HEADERS_CAR[column]);
+            }
+
+            int rowId = 1;
+            for (Car car: cars) {
+                Row row = sheet.createRow(rowId++);
+
+                row.createCell(0).setCellValue(car.getId());
+                row.createCell(1).setCellValue(car.getModel());
+                row.createCell(2).setCellValue(car.getDoors());
+                row.createCell(3).setCellValue(car.getSeats());
+                row.createCell(4).setCellValue(car.getLuggage());
+                row.createCell(5).setCellValue(car.getTransmission());
+                row.createCell(6).setCellValue(car.getAirConditioning());
+                row.createCell(7).setCellValue(car.getAge());
+                row.createCell(8).setCellValue(car.getPricePerHour());
+                row.createCell(9).setCellValue(car.getFuelType());
+            }
+            workbook.write(out);
+            return new ByteArrayInputStream(out.toByteArray());
+        }catch (IOException e){
             throw new RuntimeException("fail to import data to Excel file: " + e.getMessage());
         }
     }
